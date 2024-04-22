c73b038a4fe6a8e5a1e62051d19c7b8517e680b3
PlaksinaKate <plaksina.larisa@yandex.ru>
2024-04-22 21:05:27 +0700

    feat: add github page

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -3,7 +3,7 @@ import { store } from "../store";
 const sendForm = async () => {
   const { name, phone } = store.getState().form;
 
-  const response = await fetch(`"https://api.ru/?name=${name}&phone=${phone}`)
+  return await fetch(`"https://api.ru/?name=${name}&phone=${phone}`)
     .then((response) => console.log(response.json()))
     .catch((error) => {
       const errorMessage: string = error.message;
